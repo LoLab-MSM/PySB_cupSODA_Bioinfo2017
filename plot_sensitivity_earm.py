5e7c80ba7481ccec949008e55d90ee8a0d5ed16a
jamespino <james.c.pino@vanderbilt.edu>
2016-07-14 17:18:52 -0500

    Removed an image creation that doesn't exist

--- plot_sensitivity_earm.py
+++ plot_sensitivity_earm.py
@@ -22,7 +22,6 @@ def create_earm_boxplot(sensitivity_matrix, save_name):
     median = int(np.median(range(0, len(vals))))
 
     all_runs_1 = []
-    sensitivity_matrix[sensitivity_matrix!=0] = sensitivity_matrix[sensitivity_matrix!=0]-100
     length_matrix = len(sensitivity_matrix)
     len_vals = len(vals)
     for j in range(0, length_matrix, len_vals):
@@ -42,7 +41,7 @@ def create_earm_boxplot(sensitivity_matrix, save_name):
 
     v_max = max(np.abs(sensitivity_matrix.min()), sensitivity_matrix.max())
     v_min = -1 * v_max
-    print(v_max,v_min)
+
     n = len(sensitivity_matrix)
     im = ax1.imshow(sensitivity_matrix, interpolation='nearest',
                     origin='lower', cmap=plt.get_cmap(colors),
@@ -85,6 +84,8 @@ def create_earm_boxplot(sensitivity_matrix, save_name):
     plt.subplots_adjust(top=0.95, hspace=.2)
 
     plt.savefig(os.path.join('Figures', '%s.png') % save_name, bbox_tight='True', dpi=300)
+    plt.savefig(os.path.join('Figures', '%s.eps') % save_name, bbox_tight='True')
+    plt.savefig(os.path.join('Figures', '%s.pdf') % save_name, bbox_tight='True')
 
 
 def create_supplement_figure_9():
@@ -96,11 +97,7 @@ def create_supplement_figure_10():
     data = np.loadtxt(os.path.join('SensitivityData', 'sens_earm_parameter_set_2.csv'))
     create_earm_boxplot(data, 'supp_figure_10_earm_parameter_set_2')
 
-def create_supplement_figure_11():
-    data = np.loadtxt(os.path.join('SensitivityData', 'earm_parameters_1_gpu_new_image_matrix.csv'))
-    create_earm_boxplot(data, 'supp_figure_11_earm_parameter_set_1')
 
 if __name__ == '__main__':
-    #create_supplement_figure_9()
-    #create_supplement_figure_10()
-    create_supplement_figure_11()
+    create_supplement_figure_9()
+    create_supplement_figure_10()
