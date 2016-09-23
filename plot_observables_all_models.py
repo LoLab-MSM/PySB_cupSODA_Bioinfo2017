d3cc9e213901d390733da5c828aee94da8defbdf
jamespino <james.c.pino@vanderbilt.edu>
2016-09-23 14:02:09 -0500

    Cleaned up plotting of observables. Images are used in paper.

--- plot_observables_all_models.py
+++ plot_observables_all_models.py
@@ -2,13 +2,9 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import numpy as np
 import scipy.interpolate
-
 import pysb
 import pysb.integrate as integrate
-from models.earm_lopez_embedded_flat import model as earm
-from models.ras_camp_pka import model as ras
 from pysb.bng import generate_equations
-from pysb.examples.tyson_oscillator import model as tyson
 from pysb.util import update_param_vals, load_params
 
 ATOL = 1e-12
@@ -17,20 +13,33 @@ mxstep = 20000
 
 
 def print_model_stats(model):
+    """ provides stats for the models
+
+    :param model:
+    """
     generate_equations(model)
     print("Information about model {0}".format(model.name))
     print("Number of rules {0}".format(len(model.rules)))
     print("Number of parameters {0}".format(len(model.parameters)))
-    print("Number of parameter rules {0}".format(len(model.parameters_rules())))
+    print(
+    "Number of parameter rules {0}".format(len(model.parameters_rules())))
     print("Number of reactions {0}".format(len(model.reactions)))
-    print("Number of initial conditions {0}".format(len(model.initial_conditions)))
-    print("Number of species {0}\n".format(len(model.species)))
+    print(
+    "Number of initial conditions {0}".format(len(model.initial_conditions)))
+    print("Number of species {0}".format(len(model.species)))
+    print('{}'.format('-' * 24))
 
 
+def create_supplement_figure_4():
+    """ plots cell cycle observable """
 
-def observable_cell_cycle(model, time, save_name, observable_name):
-    print_model_stats(model)
-    solver = pysb.integrate.Solver(model, time, rtol=RTOL, atol=ATOL, integrator='lsoda', mxstep=mxstep)
+    from models.tyson_oscillator_in_situ import model
+    time = np.linspace(0, 300, 1000)
+    observable_name = 'M'
+    save_name = 'supp_figure_4_tyson_output'
+
+    solver = pysb.integrate.Solver(model, time, rtol=RTOL, atol=ATOL,
+                                   integrator='lsoda', mxstep=mxstep)
     solver.run()
     out = solver.yobs[observable_name]
     out /= model.parameters['cyc0'].value
@@ -55,42 +64,62 @@ def observable_cell_cycle(model, time, save_name, observable_name):
     plt.axvline(x[1], linestyle='dashed', color='black')
     arrow_x_0 = x[1] - x[0]
     y_distance = yy[1]
-    plt.arrow(arrow_x_0, y_distance, x[1] - arrow_x_0 - 5, 0, head_width=.05, head_length=3, color='k')
-    plt.arrow(arrow_x_0, y_distance, x[0] - arrow_x_0 + 5, 0, head_width=.05, head_length=3, color='k')
+    plt.arrow(arrow_x_0, y_distance, x[1] - arrow_x_0 - 5, 0, head_width=.05,
+              head_length=3, color='k')
+    plt.arrow(arrow_x_0, y_distance, x[0] - arrow_x_0 + 5, 0, head_width=.05,
+              head_length=3, color='k')
     plt.xlim(0, 60)
     plt.ylim(0, 1.0)
-    plt.savefig('%s/%s_%s.png' % ('Figures', save_name, observable_name), dpi=150)
+    plt.savefig('%s/%s_%s.png' % ('Figures', save_name, observable_name),
+                dpi=150)
     plt.savefig('%s/%s_%s.eps' % ('Figures', save_name, observable_name))
     plt.close()
-
-
-def observable_ras(model, time, save_name, observable_name):
     print_model_stats(model)
-    solver = pysb.integrate.Solver(model, time, rtol=RTOL, atol=ATOL, integrator='lsoda', mxstep=mxstep)
+
+
+def create_supplement_figure_6():
+    from models.ras_camp_pka import model
+    time = np.linspace(0, 1500, 500)
+    observable_name = 'obs_cAMP'
+    save_name = 'supp_figure_6_ras_output'
+
+    solver = pysb.integrate.Solver(model, time, rtol=RTOL, atol=ATOL,
+                                   integrator='lsoda', mxstep=mxstep)
     solver.run()
     out = solver.yobs[observable_name]
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(time, solver.yobs[observable_name], linewidth=2, )
     max_y = np.where(out == out.max())
-    ax.plot(time[max_y], out.max(), 'o', color='red', markersize=14, mew=3, mfc='none', alpha=.75)
+    ax.plot(time[max_y], out.max(), 'o', color='red', markersize=14, mew=3,
+            mfc='none', alpha=.75)
     plt.axhline(out.max(), linestyle='dashed', color='black')
     plt.ylabel('cAMP (count) ', fontsize=16)
     plt.xlabel('Time (s)', fontsize=16)
     ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
-    plt.ylim(0, 125000)
+    plt.ylim(0, 135000)
     plt.xlim(0, 1000)
     plt.tight_layout()
-    plt.savefig('%s/%s_%s.png' % ('Figures', save_name, observable_name), dpi=150)
+    plt.savefig('%s/%s_%s.png' % ('Figures', save_name, observable_name),
+                dpi=150)
     plt.savefig('%s/%s_%s.eps' % ('Figures', save_name, observable_name))
     plt.close()
-
-
-def observable_earm(model, time, save_name, observable_name):
     print_model_stats(model)
-    solver = pysb.integrate.Solver(model, time, rtol=RTOL, atol=ATOL, integrator='lsoda', mxstep=mxstep)
+
+
+def create_supplement_figure_8():
+    """ plots EARM observable """
+    from models.earm_lopez_embedded_flat import model
+    time = np.linspace(0, 20000, 101)
+    observable_name = 'aSmac'
+    new_params = load_params('Params/earm_parameter_set_one.txt')
+    save_name = 'supp_figure_8_earm_output_parameter_set_one'
+    update_param_vals(model, new_params)
+    solver = pysb.integrate.Solver(model, time, rtol=RTOL, atol=ATOL,
+                                   integrator='lsoda', mxstep=mxstep)
     solver.run()
-    ysim_momp_norm = solver.yobs[observable_name] / np.nanmax(solver.yobs[observable_name])
+    ysim_momp_norm = solver.yobs[observable_name] / np.nanmax(
+            solver.yobs[observable_name])
     st, sc, sk = scipy.interpolate.splrep(time, ysim_momp_norm)
     try:
         t10 = scipy.interpolate.sproot((st, sc - 0.10, sk))[0]
@@ -108,32 +137,11 @@ def observable_earm(model, time, save_name, observable_name):
     plt.ylim(-.05, 1.05)
     plt.xticks(fontsize=16)
     plt.yticks(fontsize=16)
-    plt.savefig('%s/%s_%s.png' % ('Figures', save_name, observable_name), dpi=150)
+    plt.savefig('%s/%s_%s.png' % ('Figures', save_name, observable_name),
+                dpi=150)
     plt.savefig('%s/%s_%s.eps' % ('Figures', save_name, observable_name))
     plt.close()
-
-
-def create_supplement_figure_4():
-    tspan = np.linspace(0, 300, 1000)
-    observable = 'M'
-    savename = 'supp_figure_4_tyson_output'
-    observable_cell_cycle(tyson, tspan, savename, observable)
-
-
-def create_supplement_figure_6():
-    tspan = np.linspace(0, 1500, 500)
-    observable = 'obs_cAMP'
-    savename = 'supp_figure_6_ras_output'
-    observable_ras(ras, tspan, savename, observable)
-
-
-def create_supplement_figure_8():
-    tspan = np.linspace(0, 20000, 100)
-    observable = 'aSmac'
-    new_params = load_params('Params/earm_parameter_set_one.txt')
-    savename = 'supp_figure_8_earm_output_parameter_set_one_gpu_new'
-    update_param_vals(earm, new_params)
-    observable_earm(earm, tspan, savename, observable)
+    print_model_stats(model)
 
 
 if __name__ == '__main__':
