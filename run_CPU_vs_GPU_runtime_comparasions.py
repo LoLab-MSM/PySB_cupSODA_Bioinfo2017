bb87e85bff56f28cdab52195eab7882e9a15daf2
jamespino <james.c.pino@vanderbilt.edu>
2016-09-23 14:12:39 -0500

    Cleaned up and formatted main code to run benchmarks.

--- run_CPU_vs_GPU_runtime_comparasions.py
+++ run_CPU_vs_GPU_runtime_comparasions.py
@@ -9,17 +9,24 @@ ATOL = 1e-6
 RTOL = 1e-6
 mxstep = 20000
 
-card = 'gpu'
+card = 'gtx-980-ti'
 CPU = 'cpu'
 GHZ = 'speed'
 
+# mem = 0
+# global 0
+# shared 1
+# sharedconstant 2
+mem_dict = {0: 'global', 1: 'shared', 2: 'sharedconstant'}
 
-def main(tspan, model, simulations, vol, name, run):
-    scipy_output = "model,nsims,scipytime,rtol,atol,mxsteps,t_end,n_steps,cpu,GHz\n"
-    scipy_solver = pysb.integrate.Solver(model, tspan, rtol=RTOL, atol=ATOL, integrator='lsoda', )
+
+def main(tspan, model, simulations, vol, name, run, mem):
     set_cupsoda_path("/home/pinojc/git/cupSODA")
-    solver = CupSodaSolver(model, tspan, atol=ATOL, rtol=RTOL, verbose=False)
-    output = "model,nsims,tpb,mem,cupsodatime,pythontime,rtol,atol,mxsteps,t_end,n_steps,deterministic,vol,card\n"
+    solver = CupSodaSolver(model, tspan, atol=ATOL, rtol=RTOL)
+    scipy_solver = pysb.integrate.Solver(model, tspan, rtol=RTOL, atol=ATOL,
+                                         integrator='lsoda', )
+    output = "model,nsims,tpb,mem,cupsodatime,cupsoda_io_time,pythontime,rtol,atol,mxsteps,t_end,n_steps,vol,card\n"
+    scipy_output = "model,nsims,scipytime,rtol,atol,mxsteps,t_end,n_steps,cpu,GHz\n"
     generate_equations(model)
     nominal_values = np.array([p.value for p in model.parameters])
     for num_particles in simulations:
@@ -33,37 +40,41 @@ def main(tspan, model, simulations, vol, name, run):
                     if str(ic[0]) == str(model.species[j]):
                         y0[:, j] = ic[1].value
                         break
-            mem = 2
-            i = 16
+
+            tpb = 16
+            i = int(np.ceil(1. * num_particles / tpb))
             start_time = time.time()
             solver.run(param_values=c_matrix,
                        y0=y0,
                        gpu=0,
                        max_steps=mxstep,
                        vol=vol,
-                       obs_species_only=False,
-                       memory_usage='sharedconstant')
+                       n_blocks=i,
+                       # outdir='.',
+                       obs_species_only=True,
+                       memory_usage=mem_dict[mem])
             end_time = time.time()
-            new_line = '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (name,
-                                                                        num_particles,
-                                                                        str(i),
-                                                                        mem,
-                                                                        solver._total_time,
-                                                                        end_time - start_time,
-                                                                        str(RTOL),
-                                                                        str(ATOL),
-                                                                        len(tspan),
-                                                                        mxstep,
-                                                                        np.max(tspan),
-                                                                        '1',
-                                                                        vol,
-                                                                        card)
+            new_line = '%s,%s,%s,%s,%s,%s,%s,%s,%s,' \
+                       '%s,%s,%s,%s,%s\n' % (name,
+                                             num_particles,
+                                             str(tpb),
+                                             mem,
+                                             solver._cupsoda_time,
+                                             solver._total_time,
+                                             end_time - start_time,
+                                             str(RTOL),
+                                             str(ATOL),
+                                             len(tspan),
+                                             mxstep,
+                                             np.max(tspan),
+                                             vol,
+                                             card)
             output += new_line
             print(new_line)
-            print('out==\n')
+            print('out==')
             print(solver.concs_observables(squeeze=False)[0][0])
             print(solver.concs_observables(squeeze=False)[0][-1])
-            print('\n==out')
+            print('==out\n')
 
         if run == 'scipy':
             start_time = time.time()
@@ -83,46 +94,52 @@ def main(tspan, model, simulations, vol, name, run):
                                                              GHZ,)
             scipy_output += new_line
     if run == 'scipy':
-        with open('%s_scipy_timing.csv' % name, 'w') as f:
+        with open('%s_scipy_timing_%s.csv' % (name, str(mem)), 'w') as f:
             f.write(scipy_output)
     if run == 'cupSODA':
-        with open('%s_cupsoda_timing.csv' % name, 'w') as f:
+        with open('%s_cupsoda_timing_%s.csv' % (name, str(mem)), 'w') as f:
             f.write(output)
 
 
-def run_tyson(run):
+def run_tyson(run, mem):
     from models.tyson_oscillator_in_situ import model
     name = 'tyson'
     tspan = np.linspace(0, 100, 100)
-    simulations = [10, 100, 1000,  10000, 100000]
+    simulations = [10, 100, 1000, 10000, 100000]
     vol = 1e-19
-    main(tspan, model, simulations, vol, name, run)
+    main(tspan, model, simulations, vol, name, run, mem)
 
 
-def run_ras(run):
-    from models.ras_amp_pka import model
+def run_ras(run, mem):
+    from models.ras_camp_pka import model
     name = 'ras'
     tspan = np.linspace(0, 1500, 100)
     simulations = [10, 100, 1000, 10000, 100000]
     vol = 1e-19
-    main(tspan, model, simulations, vol, name, run)
+    main(tspan, model, simulations, vol, name, run, mem)
 
 
-def run_earm(run):
+def run_earm(run, mem):
     from models.earm_lopez_embedded_flat import model
     name = 'earm'
     tspan = np.linspace(0, 20000, 100)
     simulations = [10, 100, 1000, 10000]
-    vol = 1.661e-20
-    main(tspan, model, simulations, vol, name, run)
+    vol = 1.661e-19
+    main(tspan, model, simulations, vol, name, run, mem)
 
 
 if __name__ == '__main__':
     # Run all the cupsoda timing
-    run_tyson('cupSODA')
-    run_ras('cupSODA')
-    run_earm('cupSODA')
+    run_tyson('cupSODA', 0)
+    run_tyson('cupSODA', 1)
+    run_tyson('cupSODA', 2)
+    run_earm('cupSODA', 0)
+    run_earm('cupSODA', 1)
+    run_earm('cupSODA', 2)
+    run_ras('cupSODA', 0)
+    run_ras('cupSODA', 1)
+    run_ras('cupSODA', 2)
     # Run all the scipy timing
-    run_tyson("scipy")
-    run_ras("scipy")
-    run_earm("scipy")
+    # run_tyson("scipy")
+    # run_ras("scipy")
+    # run_earm("scipy",0)
