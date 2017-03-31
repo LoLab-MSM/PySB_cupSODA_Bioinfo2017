c9d63cd03dc2eb3b7609e1d69d19c3075c036a3e
James Pino <james.c.pino@vanderbilt.edu>
2017-03-31 16:03:16 -0500

    Updated run cpu and gpu timings to use new simulator formats.

--- run_CPU_vs_GPU_runtime_comparasions.py
+++ run_CPU_vs_GPU_runtime_comparasions.py
@@ -1,9 +1,12 @@
 import time
 import numpy as np
-import pysb
-import pysb.integrate as integrate
 from pysb.bng import generate_equations
-from pysb.simulator.cupsoda import set_cupsoda_path, CupSodaSolver
+from pysb.simulator.cupsoda import CupSodaSimulator
+from pysb.simulator.scipyode import ScipyOdeSimulator
+import pandas as pd
+import os
+import logging
+from pysb.logging import setup_logger
 
 ATOL = 1e-6
 RTOL = 1e-6
@@ -13,133 +16,188 @@ card = 'gtx-980-ti'
 CPU = 'cpu'
 GHZ = 'speed'
 
-# mem = 0
-# global 0
-# shared 1
-# sharedconstant 2
-mem_dict = {0: 'global', 1: 'shared', 2: 'sharedconstant'}
+mem_dict = {
+    0: 'global',
+    1: 'shared',
+    2: 'sharedconstant'
+    }
 
 
-def main(tspan, model, simulations, vol, name, run, mem):
-    set_cupsoda_path("/home/pinojc/git/cupSODA")
-    solver = CupSodaSolver(model, tspan, atol=ATOL, rtol=RTOL)
-    scipy_solver = pysb.integrate.Solver(model, tspan, rtol=RTOL, atol=ATOL,
-                                         integrator='lsoda', )
-    output = "model,nsims,tpb,mem,cupsodatime,cupsoda_io_time,pythontime,rtol,atol,mxsteps,t_end,n_steps,vol,card\n"
-    scipy_output = "model,nsims,scipytime,rtol,atol,mxsteps,t_end,n_steps,cpu,GHz\n"
+def run_scipy(tspan, model, simulations, name, ):
+    integrator_option = dict(rtol=RTOL, atol=ATOL, mxstep=mxstep)
+    scipy_solver = ScipyOdeSimulator(model, tspan,
+                                     integrator_options=integrator_option,
+                                     integrator='lsoda', )
+
+    cols = ['model', 'nsims', 'scipytime', 'rtol', 'atol', 'mxsteps', 't_end',
+            'n_steps', 'cpu', 'GHz', ]
+    all_output = []
+    for num_particles in simulations:
+        out_list = []
+        start_time = time.time()
+        for i in range(num_particles):
+            x = scipy_solver.run()
+        total_time = time.time() - start_time
+        out_list.append(name)
+        out_list.append(num_particles)
+        out_list.append(total_time)
+        out_list.append(RTOL)
+        out_list.append(ATOL)
+        out_list.append(mxstep)
+        out_list.append(np.max(tspan))
+        out_list.append(len(tspan))
+        out_list.append(CPU)
+        out_list.append(GHZ)
+        print(" ".join(str(i) for i in out_list))
+        all_output.append(out_list)
+    df = pd.DataFrame(all_output, columns=cols)
+    df.to_csv('{}_scipy_timings.csv'.format(name), index=False)
+    return df
+
+
+def run_cupsoda(tspan, model, simulations, vol, name, mem):
+    tpb = 32
+
+    solver = CupSodaSimulator(model, tspan, atol=ATOL, rtol=RTOL, gpu=0,
+                              max_steps=mxstep, verbose=False, vol=vol,
+                              obs_species_only=True,
+                              memory_usage=mem_dict[mem])
+
+    cols = ['model', 'nsims', 'tpb', 'mem', 'cupsodatime', 'cupsoda_io_time',
+            'pythontime', 'rtol', 'atol', 'mxsteps', 't_end', 'n_steps', 'vol',
+            'card']
     generate_equations(model)
     nominal_values = np.array([p.value for p in model.parameters])
+    all_output = []
     for num_particles in simulations:
+        # set the number of blocks to make it 16 threads per block
+        n_blocks = int(np.ceil(1. * num_particles / tpb))
+        solver.n_blocks = n_blocks
 
-        if run == "cupSODA":
-            c_matrix = np.zeros((num_particles, len(nominal_values)))
-            c_matrix[:, :] = nominal_values
-            y0 = np.zeros((num_particles, len(model.species)))
-            for ic in model.initial_conditions:
-                for j in range(len(model.species)):
-                    if str(ic[0]) == str(model.species[j]):
-                        y0[:, j] = ic[1].value
-                        break
+        # create a matrix of initial conditions
+        c_matrix = np.zeros((num_particles, len(nominal_values)))
+        c_matrix[:, :] = nominal_values
+        y0 = np.zeros((num_particles, len(model.species)))
+        for ic in model.initial_conditions:
+            for j in range(len(model.species)):
+                if str(ic[0]) == str(model.species[j]):
+                    y0[:, j] = ic[1].value
+                    break
 
-            tpb = 16
-            i = int(np.ceil(1. * num_particles / tpb))
-            start_time = time.time()
-            solver.run(param_values=c_matrix,
-                       y0=y0,
-                       gpu=0,
-                       max_steps=mxstep,
-                       vol=vol,
-                       n_blocks=i,
-                       # outdir='.',
-                       obs_species_only=True,
-                       memory_usage=mem_dict[mem])
-            end_time = time.time()
-            new_line = '%s,%s,%s,%s,%s,%s,%s,%s,%s,' \
-                       '%s,%s,%s,%s,%s\n' % (name,
-                                             num_particles,
-                                             str(tpb),
-                                             mem,
-                                             solver._cupsoda_time,
-                                             solver._total_time,
-                                             end_time - start_time,
-                                             str(RTOL),
-                                             str(ATOL),
-                                             len(tspan),
-                                             mxstep,
-                                             np.max(tspan),
-                                             vol,
-                                             card)
-            output += new_line
-            print(new_line)
-            print('out==')
-            print(solver.concs_observables(squeeze=False)[0][0])
-            print(solver.concs_observables(squeeze=False)[0][-1])
-            print('==out\n')
+        # setup a unique log output file to extract timing from
+        log_file = 'logfile_{}.log'.format(num_particles)
+        # remove it if it already exists (happens if rerunning simulation)
+        if os.path.exists(log_file):
+            os.remove(log_file)
 
-        if run == 'scipy':
-            start_time = time.time()
-            for i in xrange(num_particles):
-                scipy_solver.run()
-            total_time = time.time() - start_time
-            print('sim = %s , time = %s sec' % (num_particles, total_time))
-            new_line = '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\n' % (name,
-                                                             num_particles,
-                                                             total_time,
-                                                             RTOL,
-                                                             ATOL,
-                                                             mxstep,
-                                                             np.max(tspan),
-                                                             len(tspan),
-                                                             CPU,
-                                                             GHZ,)
-            scipy_output += new_line
-    if run == 'scipy':
-        with open('%s_scipy_timing_%s.csv' % (name, str(mem)), 'w') as f:
-            f.write(scipy_output)
-    if run == 'cupSODA':
-        with open('%s_cupsoda_timing_%s.csv' % (name, str(mem)), 'w') as f:
-            f.write(output)
+        setup_logger(logging.INFO, file_output=log_file, console_output=True)
+        start_time = time.time()
+
+        # run the simulations
+        x = solver.run(param_values=c_matrix, initials=y0)
+
+        end_time = time.time()
+
+        # create an emtpy list to put all data for this run
+        out_list = list()
+        out_list.append(name)
+        out_list.append(num_particles)
+        out_list.append(str(tpb))
+        out_list.append(mem)
+        cupsoda_time = 'error'
+        total_time = 'error'
+        with open(log_file, 'r') as f:
+            for line in f:
+                if 'reported time' in line:
+                    good_line = line.split(':')[-1].split()[0]
+                    cupsoda_time = float(good_line)
+                if 'I/O time' in line:
+                    good_line = line.split(':')[-1].split()[0]
+                    total_time = float(good_line)
+            f.close()
+
+        out_list.append(cupsoda_time)
+        out_list.append(total_time)
+        out_list.append(end_time - start_time)
+        out_list.append(RTOL)
+        out_list.append(ATOL)
+        out_list.append(len(tspan))
+        out_list.append(mxstep)
+        out_list.append(np.max(tspan))
+        out_list.append(vol)
+        out_list.append(card)
+        all_output.append(out_list)
+        print(" ".join(str(i) for i in out_list))
+        print('out==')
+        print(x.observables[0][0])
+        print(x.observables[0][-1])
+        print('==out\n')
+
+    df = pd.DataFrame(all_output, columns=cols)
+    print(df)
+    df.to_csv('{}_cupsoda_timings_{}.csv'.format(name, mem), index=False)
+    return df
 
 
-def run_tyson(run, mem):
+def run_tyson(run, mem=None):
     from models.tyson_oscillator_in_situ import model
     name = 'tyson'
     tspan = np.linspace(0, 100, 100)
     simulations = [10, 100, 1000, 10000, 100000]
+    # simulations = [10, 100, 1000, 10000]
     vol = 1e-19
-    main(tspan, model, simulations, vol, name, run, mem)
+    if run == 'cupSODA':
+        return run_cupsoda(tspan, model, simulations, vol, name, mem)
+    else:
+        return run_scipy(tspan, model, simulations, name)
 
 
-def run_ras(run, mem):
+def run_ras(run, mem=None):
     from models.ras_camp_pka import model
     name = 'ras'
     tspan = np.linspace(0, 1500, 100)
     simulations = [10, 100, 1000, 10000, 100000]
     vol = 1e-19
-    main(tspan, model, simulations, vol, name, run, mem)
+    if run == 'cupSODA':
+        return run_cupsoda(tspan, model, simulations, vol, name, mem)
+    else:
+        return run_scipy(tspan, model, simulations, name)
 
 
-def run_earm(run, mem):
+def run_earm(run, mem=None):
     from models.earm_lopez_embedded_flat import model
     name = 'earm'
     tspan = np.linspace(0, 20000, 100)
     simulations = [10, 100, 1000, 10000]
+    # simulations = [10000]
     vol = 1.661e-19
-    main(tspan, model, simulations, vol, name, run, mem)
+    if run == 'cupSODA':
+        return run_cupsoda(tspan, model, simulations, vol, name, mem)
+    else:
+        return run_scipy(tspan, model, simulations, name)
 
 
 if __name__ == '__main__':
     # Run all the cupsoda timing
-    run_tyson('cupSODA', 0)
-    run_tyson('cupSODA', 1)
-    run_tyson('cupSODA', 2)
-    run_earm('cupSODA', 0)
-    run_earm('cupSODA', 1)
-    run_earm('cupSODA', 2)
-    run_ras('cupSODA', 0)
-    run_ras('cupSODA', 1)
-    run_ras('cupSODA', 2)
+    tyson_cupsoda = []
+    for i in range(3):
+        tyson_cupsoda.append(run_tyson('cupSODA', i))
+    df = pd.concat(tyson_cupsoda)
+    df.to_csv('all_tyson.csv')
+
+    earm_cupsoda = []
+    for i in range(3):
+        earm_cupsoda.append(run_earm('cupSODA', i))
+    df = pd.concat(earm_cupsoda)
+    df.to_csv('all_earm.csv')
+
+    ras_cupsoda = []
+    for i in range(3):
+        ras_cupsoda.append(run_ras('cupSODA', i))
+    df = pd.concat(ras_cupsoda)
+    df.to_csv('all_ras.csv')
+
     # Run all the scipy timing
-    # run_tyson("scipy")
-    # run_ras("scipy")
-    # run_earm("scipy",0)
+    run_tyson("scipy")
+    run_ras("scipy")
+    run_earm("scipy")
