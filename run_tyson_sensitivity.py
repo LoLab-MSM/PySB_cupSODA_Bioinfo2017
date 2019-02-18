348a68ab66d6c7abed221c73d5e0effab46f94c4
ildefog <ildefog@mule.dhcp.mc.vanderbilt.edu>
2019-02-18 18:29:40 +0000

    tesitng param sense tyson

--- run_tyson_sensitivity.py
+++ run_tyson_sensitivity.py
@@ -51,7 +51,7 @@ def run():
            scipy_solver,
             values_to_sample=vals,
             observable=observable,
-            objective_function=obj_func_cell_cycle, sens_type='initials')
+            objective_function=obj_func_cell_cycle, sens_type='params')
 
     sens.run(save_name=savename, out_dir=directory)
 
